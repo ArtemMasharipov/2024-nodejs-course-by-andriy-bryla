13b086fb7401782fc47b76456b77d274f3e6d87b
Artem Masharipov <112805569+ArtemMasharipov@users.noreply.github.com>
2024-11-24 15:56:30 +0100

    Update upload-handler.mjs

--- hw_lesson_8/services/upload-handler.mjs
+++ hw_lesson_8/services/upload-handler.mjs
@@ -1,11 +1,10 @@
 import multer from 'multer';
 
-// Настройка multer для хранения в памяти
 const storage = multer.memoryStorage();
 
 const upload = multer({
 	storage,
-	limits: { fileSize: 5 * 1024 * 1024 }, // Ограничение на размер файла (5 MB)
+	limits: { fileSize: 5 * 1024 * 1024 },
 }).single('image');
 
 export default upload;
