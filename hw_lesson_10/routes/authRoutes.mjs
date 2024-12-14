74b35f53e7e7419c79fc6c4cebc20ee10116b851
Artem Masharipov <112805569+ArtemMasharipov@users.noreply.github.com>
2024-12-14 21:37:17 +0100

    Update authRoutes.mjs

--- hw_lesson_10/routes/authRoutes.mjs
+++ hw_lesson_10/routes/authRoutes.mjs
@@ -3,9 +3,9 @@ import AuthController from '../controllers/authController.mjs';
 
 const router = Router();
 
-router.get('/login', AuthController.renderLogin); // Форма входа
-router.post('/login', AuthController.login); // Обработка входа
+router.get('/login', AuthController.renderLogin);
+router.post('/login', AuthController.login);
 
-router.get('/logout', AuthController.logout); // Обработка выхода
+router.get('/logout', AuthController.logout);
 
 export default router;
