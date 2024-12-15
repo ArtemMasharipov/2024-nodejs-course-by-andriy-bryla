c0c848159dc7af4495042b72b86f092eb6ab4093
Artem Masharipov <112805569+ArtemMasharipov@users.noreply.github.com>
2024-12-15 11:04:59 +0100

    Update validate-request-handler.mjs

--- hw_lesson_10/services/validate-request-handler.mjs
+++ hw_lesson_10/services/validate-request-handler.mjs
@@ -1,19 +1,20 @@
 import { validationResult } from 'express-validator';
 
-const validateRequest = (formView, getAdditionalData = null) => [
-	async (req, res, next) => {
-		const errors = validationResult(req);
-		if (!errors.isEmpty()) {
-			const additionalData = getAdditionalData ? await getAdditionalData(req) : {};
-			const data = {
-				...req.body,
-				...additionalData,
-				errors: errors.array(),
-			};
-			return res.status(400).render(formView, data);
-		}
-		next();
-	},
+const validateRequest = (formView, validationSchema, getAdditionalData = null) => [
+    validationSchema,
+    async (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            const additionalData = getAdditionalData ? await getAdditionalData(req) : {};
+            const data = {
+                ...req.body,
+                ...additionalData,
+                errors: errors.array(),
+            };
+            return res.status(400).render(formView, data);
+        }
+        next();
+    },
 ];
 
 export default validateRequest;
